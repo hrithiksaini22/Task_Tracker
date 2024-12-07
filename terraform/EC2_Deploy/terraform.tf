e6643d06fece1f924e69799ec93045de696b0fa0
hrithik <122617770+hrithiksaini22@users.noreply.github.com>
2024-12-06 19:17:33 -0500

    Update terraform.tf

--- terraform/EC2_Deploy/terraform.tf
+++ terraform/EC2_Deploy/terraform.tf
@@ -2,6 +2,15 @@ terraform {
   backend "local" {
     path = "terraform.tfstate"
   }
+/* you can also specify s3 as backend using s3 and dynamodb for state locking
+backend "s3" {
+    bucket         = "my-tf-test-bucket123321"  // bucket name
+    key            = "prod/aws_infra" // path where terraform will save the file inside bucket
+    region         = "us-west-1"
+    dynamodb_table = "terraform-locks" // for locking the state file
+    encrypt        = true
+  } 
+*/
   required_version = ">= 1.0.0"
   required_providers {
     aws = {
